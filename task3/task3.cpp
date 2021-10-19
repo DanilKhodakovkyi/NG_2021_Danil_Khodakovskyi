f42c1093d9f63b2b173ff08c4924a8c3885a76d3
DanilKhodakovskyi <gogota7091@gmail.com>
2021-10-19 22:34:24 +0300

    added task3

---
+++ task3/task3.cpp
@@ -0,0 +1,16 @@
+﻿using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Enter value a:" << endl;
+    cin >> a;
+    cout << "Enter value b:" << endl;
+    cin >> b;
+    cout << endl;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "Your numbers" << endl << a << endl << b << endl;
+}
